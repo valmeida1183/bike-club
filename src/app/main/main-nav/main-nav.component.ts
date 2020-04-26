85af738c8ba6529bd82574754c6954f0d4639296
Vinicius Almeida <termobin@gmail.com>
2020-04-25 21:52:07 -0300

    fixed sidenav

--- src/app/main/main-nav/main-nav.component.ts
+++ src/app/main/main-nav/main-nav.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable, Subscription } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, tap } from 'rxjs/operators';
 
 import { AuthService } from 'src/app/auth/auth.service';
 import { Role } from 'src/app/models/auth/role.model';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'bc-main-nav',
@@ -12,13 +13,20 @@ import { Role } from 'src/app/models/auth/role.model';
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent implements OnInit, OnDestroy {
+  @ViewChild('drawer', {static: false}) drawer: MatSidenav;
   role: Role;
   userName: string;
+
   private authUserDataSubscription: Subscription;
   private userSubscription: Subscription;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
+      tap(result => {
+        if (!result.matches && this.drawer) {
+          this.drawer.close();
+        }
+      }),
       map(result => result.matches),
       shareReplay()
     );

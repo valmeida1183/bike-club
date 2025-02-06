6cd6bcb46be0d366bf5dd38da4372283330a9861
Vinicius Lopes de Almeida <termobin@gmail.com>
2025-02-06 13:32:01 -0300

    Updated Angular to version 16

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card';
+
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatLegacyDialogModule as MatDialogModule, MAT_LEGACY_DIALOG_DEFAULT_OPTIONS as MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/legacy-dialog';
-import { MatExpansionModule, MatExpansionPanel, MatExpansionPanelTitle } from '@angular/material/expansion';
+import {
+  MatLegacyDialogModule as MatDialogModule,
+  MAT_LEGACY_DIALOG_DEFAULT_OPTIONS as MAT_DIALOG_DEFAULT_OPTIONS,
+} from '@angular/material/legacy-dialog';
+import {
+  MatExpansionModule,
+  MatExpansionPanel,
+  MatExpansionPanelTitle,
+} from '@angular/material/expansion';
 import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
@@ -26,50 +32,50 @@ import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/le
 import { SimpleDialogComponent } from './simple-dialog/simple-dialog.component';
 import { TitleComponent } from './title/title.component';
 import { PanelComponent } from './panel/panel.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-    declarations: [SimpleDialogComponent, TitleComponent, PanelComponent],
-    imports: [
-        CommonModule,
-        FlexLayoutModule,
-        MatButtonModule,
-        MatCardModule,
-        MatDialogModule,
-        MatIconModule,
-        MatExpansionModule
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    exports: [
-        CommonModule,
-        FlexLayoutModule,
-        MatButtonModule,
-        MatCardModule,
-        MatDialogModule,
-        MatToolbarModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatBadgeModule,
-        MatListModule,
-        MatGridListModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatRadioModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatChipsModule,
-        MatTooltipModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatProgressSpinnerModule,
-        MatExpansionModule,
-        TitleComponent,
-        PanelComponent
-    ],
-    providers: [
-        { provide: MAT_DIALOG_DEFAULT_OPTIONS,
-            useValue: { minWidth: '15vw', minHeight: '10vh', autoFocus: false }
-        }
-    ]
+  declarations: [SimpleDialogComponent, TitleComponent, PanelComponent],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatExpansionModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatListModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    TitleComponent,
+    PanelComponent,
+  ],
+  providers: [
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { minWidth: '15vw', minHeight: '10vh', autoFocus: false },
+    },
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}

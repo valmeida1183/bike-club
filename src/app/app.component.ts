95e220d2235fa3dea9ad68d14982d4b755e92fe1
Vinicius Lopes de Almeida <termobin@gmail.com>
2025-02-09 22:38:11 -0300

    Fixed layout minor issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,24 @@ import { AuthWebService } from './auth/auth-web.service';
 @Component({
   selector: 'bc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy   {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'bike-club';
   loadingSubscription: Subscription;
   isLoading = false;
 
-  constructor(private spinnerService: SpinnerService, private authWebService: AuthWebService) {}
+  constructor(
+    private spinnerService: SpinnerService,
+    private authWebService: AuthWebService,
+  ) {}
 
   ngOnInit(): void {
-    this.loadingSubscription = this.spinnerService.isLoading.subscribe(toggleValue => {
-      this.isLoading = toggleValue;
-    });
+    this.loadingSubscription = this.spinnerService.isLoading.subscribe(
+      (toggleValue) => {
+        this.isLoading = toggleValue;
+      },
+    );
 
     this.authWebService.autoSignIn();
   }
